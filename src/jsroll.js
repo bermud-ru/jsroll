81150d05610678c467596a8fb079cfc0ad887aeb
anovikov <anovikov@aif.ru>
2016-12-22 00:24:42 +0300

    21/12/2016 update 0.1.1

--- src/jsroll.js
+++ src/jsroll.js
@@ -365,7 +365,7 @@
      */
     var tmpl = function tmpl( str, data, cb, opt ) {
         var compile = function( str ) {
-            var source = str.replace(/(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)/igm, '');
+            var source = str.replace(/(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)/igm, '').trim();
             return source.length ? new Function('_e',"var p=[], print=function(){ p.push.apply(p,arguments); };with(_e){p.push('"+
                     source.replace(/[\r\t\n]/g," ").split("{%").join("\t").replace(/((^|%})[^\t]*)'/g,"$1\r")
                     .replace(/\t=(.*?)%}/g,"',$1,'").split("\t").join("');").split("%}").join("p.push('").split("\r")
@@ -382,13 +382,14 @@
 
                 var result = null, pattern = null;
                 try {
-                    if ( opt && typeof opt.before == 'function' ) str = opt.before.call(g.tmpl, str, data) || str;
+                    var after = undefined, before = undefined, pig = g.document.getElementById(id);
+                    if (pig && (before = pig.getAttribute('tmpl-before'))) eval(before);
                     pattern = compile( str );
                     if (isId) g.tmpl.cache[id] = pattern;
                     result = pattern.call(g.tmpl, data || {});
                     if (typeof cb == 'function') {
                         cb.call(pattern || g.tmpl, result);
-                        if ( opt && typeof opt.after == 'function' ) opt.after.call(g.tmpl, result, data);
+                        if (pig && (after = pig.getAttribute('tmpl-after'))) eval(after);
                     }
                 } catch( e ) {
                     console.error( e );
